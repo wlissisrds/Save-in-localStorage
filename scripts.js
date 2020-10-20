9c8e2bb01dfd6d9a315e9696114060791549f04a
wlissis <wlissisrds@outlook.com>
2020-10-19 23:27:14 -0300

    refatorando codigo

--- scripts.js
+++ scripts.js
@@ -1,25 +1,23 @@
 //se favorites nao existir inicia com array vazio
 let favorites = JSON.parse(localStorage.getItem('favorites')) || [];
+const imageContainer = document.querySelector('.image')
 
 
-//pegar imagem externa
-async function getExternalImage() {
 
-    try {
-        const reponse =  await fetch('https://source.unsplash.com/random');
-    
-        document.querySelector('.image').innerHTML = `<img src="${reponse.url}">`
-
-    }catch(err) {
-        console.error(err)
-    }
-}
-getExternalImage();
 //clicar no botao, pegar imagem externa
-document.querySelector('button').addEventListener('click', getExternalImage);
+document.querySelector('button').onclick = () => updateImage();
 
 //clicar na imagem
-document.querySelector('.image').onclick = function() {
+imageContainer.onclick = () => updateAll();
+
+// Methods
+
+function updateAll() {
+    updateFavorites();
+    updateClasses();
+}
+
+function updateFavorites() {
     //salvar no localStorage ou remover
     const imageSource = document.querySelector('.image img').src
 
@@ -27,14 +25,49 @@ document.querySelector('.image').onclick = function() {
     //indexOf() -> devolve a posição do array do imageSoucer
     const index = favorites.indexOf(imageSource);
     const existsInLocalStorage = index != -1;
+
     if (existsInLocalStorage) {
         //remove apartir do inicio -> splice(inicio, quantidade)
         favorites.splice(index, 1);
-    }else { //salvar se nao tiver no localStorage
+
+    } else { //salvar se nao tiver no localStorage
         favorites.push(imageSource);
     }
 
+
     localStorage.setItem('favorites', JSON.stringify(favorites))
-
-
 }
+
+function updateClasses() {
+    const imageSource = document.querySelector('.image img').src
+
+    const index = favorites.indexOf(imageSource);
+    const existsInLocalStorage = index != -1;
+
+    if (existsInLocalStorage) {
+        //remove a classe fav de favorito
+        imageContainer.classList.remove('fav');
+
+    } else {
+        imageContainer.classList.add('fav');
+    }
+}
+
+async function getExternalImage() {
+    try {
+        const reponse = await fetch('https://source.unsplash.com/random');
+
+        imageContainer.innerHTML = `<img src="${reponse.url}">`
+
+    } catch (err) {
+        console.error(err)
+    }
+}
+getExternalImage();
+
+
+function updateImage() {
+    getExternalImage();
+    updateClasses();
+}
+
